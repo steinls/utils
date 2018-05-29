fcd6486ccf2cbbe1497a465af7785a7cca9299d1
steinls <3026235413@qq.com>
2018-05-29 11:08:11 +0800

    添加url地址获取函数

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 //节流函数
-export function debounce(func, delay) {
+function debounce(func, delay) {
   var timer;
   return function () {
     if (timer) {
@@ -11,4 +11,21 @@ export function debounce(func, delay) {
   }
 }
 
+//获取url中的参数
+function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+	var r = window.location.search.slice(1).match(reg); //匹配目标参数
+	if (r != null) return decodeURIComponent(r[2]);
+	return null;
+}
 
+//将url转为对象
+function getUrlObj(){
+	var ret = {};
+	var search = location.search.slice(1);
+	search.replace(/([^?&]+)=([^?&]+)/g, function(s, v, k) {
+	    ret[v] = decodeURIComponent(k);
+	    return k + '=' +  v;
+	})
+	return ret;
+}
