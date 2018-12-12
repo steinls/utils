ab9ab46259bbbc673d3106fd1dd25603e54e2d6a
steinls <3026235413@qq.com>
2018-12-12 16:48:34 +0800

    更新

--- utils.js
+++ utils.js
@@ -5,22 +5,23 @@
  * @param  {Object} context 上下文对象
  * @return {Function}       延迟执行函数
  */
-export let debounce = (func = ()=>{}, delay = 700, context = this, ...args) => {
-	return () => {
-		func.id && clearTimeout(func.id)
+function debounce(func, delay, context) {
+	return function () {
+		func.id && clearTimeout(func.id);
 		func.id = setTimeout(() => {
-			func.apply(context, args)
+			func.apply(context,arguments)
 		}, delay)
 	}
 }
 
+
 /**
- * URL地址分割
+ * URL地址分割成对象
  * @param  {String} url URL地址
  * @return {Array}     对象(协议，主机名，端口号，路径部分，查询字符串)
  */
-export let urlSplit = (url) => {
-	let obj = {
+function urlSplit(url){
+	var obj = {
 		href: '123',
 		protocol: '',
 		host:'',
@@ -28,10 +29,11 @@ export let urlSplit = (url) => {
 		pathname:'',
 		search:''
 	};
-	let urlArr = url.match(/(\w+):\/\/([^/:]+)(:\d*)?([^#\?]*)([^#]*)?/);
+
+	var urlArr = url.match(/(\w+):\/\/([^/:]+)(:\d*)?([^#\?]*)([^#]*)?/);
 	
-	let idx = 0;
-	for (let key in obj) {
+	var idx = 0;
+	for (var key in obj) {
 		obj[key] = urlArr[idx];
 		idx++;
 	}
@@ -39,14 +41,15 @@ export let urlSplit = (url) => {
 	return obj;
 }
 
+
 /**
  * 将查询字符串转为对象
  * @param  {String} search 查询字符串
  * @return {Object}        对象
  */
-export let searchToObj = (search) => {
-	let obj = {}
-	search.replace(/([^?&]+)=([^?&]+)/g, (str, key, value) => {
+function searchToObj(search){
+	var obj = {}
+	search.replace(/([^?&]+)=([^?&]+)/g, function(str, key, value){
 	    // str匹配的字符串，key键名，value键值
 	    obj[key] = decodeURIComponent(value);
 	    return value + '=' +  key
@@ -54,12 +57,13 @@ export let searchToObj = (search) => {
 	return obj
 }
 
+
 /**
  * 从查询字符串中获取指定名称参数
  * @param  {String} search 查询字符串
  * @return {String}        字符串
  */
-export let searchGetParam = (name, search) => {
+function searchGetParam(name, search){
 	//构造一个含有目标参数的正则表达式对象
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
 	//匹配目标参数
@@ -68,15 +72,17 @@ export let searchGetParam = (name, search) => {
 	return null;
 }
 
+
  /**
   * 获取URL地址中的参数
   * @param  {String} options.name  参数名，不填则返回对象
   * @param  {String} options.url   url地址
   * @return {String/Object}  参数值/对象
   */
-export let getUrlData = ({name = '', url} = {}) => {
-	let href = url||location.href;
-	let search = urlSplit(href).search;
+ function getUrlData(name, url){
+ 	var name = name||'';
+	var href = url||location.href;
+	var search = urlSplit(href).search;
 
 	if (!search) {
 		console.warn('function getUrlParam 警告：URL中没有查询字符串！');
@@ -87,44 +93,36 @@ export let getUrlData = ({name = '', url} = {}) => {
 	return searchToObj(search);
 }
 
+
 /**
  * 拼接URL地址
  * @param  {Object} option 参数对象
  * @return {String}     拼接好的字符串
  */
-export let param = (option) => {
-	var search = ''
-	for(let i in option){
-		var key = encodeURIComponent(i)
-		var value = option[i]?encodeURIComponent(option[i]):''
-		search+=`&${key}=${value}`
+function param(option){
+	var search = '';
+	for(i in option){
+		var key = encodeURIComponent(i);
+		var value = option[i]?encodeURIComponent(option[i]):'';
+		search+='&'+key+'='+value;
 	}
-	// 去除开头的&
-	return search?search.slice(1):''
-}
-
-/**
- * 对象深拷贝
- * @param  {Object} obj1 要拷贝的对象
- * @return {Object}      拷贝的对象
- */
-export let deepClone = (obj1) => {
-	var obj2 = obj1.constructor === Array ? [] : {};
-	for (let key in obj1) {
-		// 如果没这个属性就结束本次循环
-		if (!obj1.hasOwnProperty(key)) {
-			continue;
-		}
-		// 是对象就递归一下，不然就直接赋值
-		if (typeof obj1[key] === 'Object') {
-			obj2[key] = deepClone(obj1[key]);
-		} else {
-			obj2[key] = obj1[key];
-		}
-	}
-	return obj2;
+	return search?search.slice(1):'';
 }
 
 
-
-
+//寄生组合式继承
+//在子类中想要调用父类的方法可以使用
+//父类.方法.apply(this,arguments)
+Function.prototype.Inherit = function(parent){
+	//非函数类型不做处理
+	if (typeof parent != 'function') return this;
+	// 对象冒充
+	parent.call(this);
+	//创建一个没有实例方法的类
+	//这样，在调用两次父类的构造的时候，就不会初始化两次实例方法/属性，避免的组合继承的缺点
+	var F=function(){}
+	F.prototype=parent.prototype;
+	this.prototype=new F();
+	// 修改指针
+	this.prototype.constructor=this;
+}
